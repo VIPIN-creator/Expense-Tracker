4dcfed6287c3f43ee83ccde25e9b8b92c9fda231
Vipin <vipintripathi121@gmail.com>
2021-02-21 13:03:27 +0530

    add main component structure

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from 'react'
 import { Grid } from '@material-ui/core'
 
 import Details from "./Components/Details/Details"
+import Main from "./Components/Main/Main"
 import useStyles from './styles'
 
 const App = () => {
     const classes = useStyles();
     return (
-        <Grid className = {classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}} >
-            <Grid item xs={12} sm={4} >
+        <Grid className = {classes.grid} container spacing={0} alignItems="center"  justify="center" style={{ height: '100vh'}} >
+            <Grid item  xs={12} sm={4} >
                 <Details title = 'Income'/>
             </Grid>
             <Grid item xs={12} sm={3} >
-                Main
+                <Main />
             </Grid>
             <Grid item xs={12} sm={4} >
                 <Details title ='Expense' />
